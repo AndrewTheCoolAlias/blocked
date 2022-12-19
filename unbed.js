fbf2351d96fef4627114aac9766b9b235c1debe3
AndrewTheCoolAlias <andrewthecoolalias@gmail.com>
2022-12-19 15:03:58 -0800

    add unbed.js

---
+++ unbed.js
@@ -0,0 +1,14 @@
+//Checking if the page is loaded in an iframe
+if(window.self != window.top) {
+//Analyzing URL to check embed link
+try {
+if(window.parent.location.hostname.indexOf("https://andrewthecoolalias.github.io") == -1) {
+window.location.href = "https://andrewthecoolalias.github.io";
+} else {
+//iFrame from host site
+}
+} catch (ex) {
+//Blocking Embed...
+window.location.href = "https://andrewthecoolalias.github.io/blocked";
+}
+} 
